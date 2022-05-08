735c2f2bcd3e537e7478b2944a74113e5c724f81
jesus1811 <jayarza1811@gmail.com>
2022-05-07 20:49:29 -0500

    update ortografia

--- components/modules/about/About.tsx
+++ components/modules/about/About.tsx
@@ -1,9 +1,4 @@
-import {
-  ContainerPrimary,
-  LabelMain,
-  LinkPrimary,
-  LinkTransparent,
-} from "../../common";
+import { ContainerPrimary, LabelMain, LinkPrimary, LinkTransparent } from "../../common";
 import styles from "./about.module.scss";
 
 export const About = () => {
@@ -14,16 +9,12 @@ export const About = () => {
         <div className={styles.container}>
           <LabelMain>Sobre Mi</LabelMain>
           <p data-aos="fade-up" className={styles.description}>
-            HELLO WORLD! I AM JESUS AYARZA Soy Front-end Developer
-            especializándome en React JS y la maquetación responsive, tengo 6
-            meses de experiencia en trabajo remoto en equipo creando
-            aplicaciones web y landing page personalizadas, me caracterizo por
-            ser autodidacta leer mucha documentación y seguir buenas practicas.
+            HELLO WORLD! I AM JESUS AYARZA Soy Front-end Developer especializándome en React JS y la maquetación
+            responsive, tengo 6 meses de experiencia en trabajo remoto en equipo creando aplicaciones web y landing page
+            personalizadas, me caracterizo por leer mucha documentación y seguir buenas prácticas.
           </p>
           <div data-aos="fade-up" className={styles.containerButton}>
-            <LinkPrimary href="https://jesus1811.github.io/cv/JesusAyarzaCV.pdf">
-              Descargar CV
-            </LinkPrimary>
+            <LinkPrimary href="https://jesus1811.github.io/cv/JesusAyarzaCV.pdf">Descargar CV</LinkPrimary>
             <LinkTransparent to="contact">Contactame</LinkTransparent>
           </div>
         </div>
